4c4ced17e1c927cfffc33d80324f3fb31bbccaa1
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-02-20 08:01:44 +0900

    show tweets!

--- renderer.js
+++ renderer.js
@@ -1,19 +1,24 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-window.jQuery = window.$ = require('./node_modules/jquery/dist/jquery.min');
+(function() {
+  window.jQuery = window.$ = require('./node_modules/jquery/dist/jquery.min');
 
-var mock = [];
-for (var i = 0; i < 100; i++) {
-  mock.push(i);
-}
+  const twitterClient = require('./twitter');
 
-var $mock = mock.map(function(number) {
-  var $tr = $('<tr></tr>');
-  var $td = $('<td></td>');
-  $td.html(number);
-  $tr.append($td);
-  return $tr;
-});
+  var element = [];
+  twitterClient.get('statuses/home_timeline', function(error, tweets, response) {
+    if(error) throw error;
+    element = tweets.map(function(tweet) { return tweet.text });
+  });
 
-$('#timeline').append($mock);
+  var $element = element.map(function(number) {
+    var $tr = $('<tr></tr>');
+    var $td = $('<td></td>');
+    $td.html(number);
+    $tr.append($td);
+    return $tr;
+  });
+
+  $('#timeline').append($element);
+}());
