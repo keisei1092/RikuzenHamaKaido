fe5880e441c52526d0b20ab0df7ac9123f770e76
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-02-20 07:53:58 +0900

    display mock elements

--- renderer.js
+++ renderer.js
@@ -2,3 +2,18 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 window.jQuery = window.$ = require('./node_modules/jquery/dist/jquery.min');
+
+var mock = [];
+for (var i = 0; i < 100; i++) {
+  mock.push(i);
+}
+
+var $mock = mock.map(function(number) {
+  var $tr = $('<tr></tr>');
+  var $td = $('<td></td>');
+  $td.html(number);
+  $tr.append($td);
+  return $tr;
+});
+
+$('#timeline').append($mock);
